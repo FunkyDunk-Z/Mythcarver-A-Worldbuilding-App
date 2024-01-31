2f9873f3114e1ee2295c5f24a4d69dc0fba16074
luke dowling <lpdowling26@googlemail.com>
2024-01-31 15:45:27 +0100

    updated the way users are updated, update the codex based on username

--- client/src/routes/MyAccountRoutes.jsx
+++ client/src/routes/MyAccountRoutes.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import { useAuthContext } from '../hooks/useAuthContext'
 
 import NotFound from '../components/utils/NotFound'
 import MyAccount from '../pages/MyAccount'
 import UpdateAccountForm from '../components/forms/UpdateAccountForm'
 
 export default function CategoryRoutes() {
-  const { user } = useAuthContext()
   return (
     <Routes>
       <Route index element={<MyAccount />} />
-      <Route path="/update" element={<UpdateAccountForm user={user} />} />
+      <Route path="/update" element={<UpdateAccountForm />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
   )
