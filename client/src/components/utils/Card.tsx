87cd1c3faba475834367035f7c7f7f1fb5e9e9a0
Duncan W. Saul <dunk.dws90@gmail.com>
2024-04-20 15:53:18 +0200

    changed card props

--- client/src/components/utils/Card.tsx
+++ client/src/components/utils/Card.tsx
@@ -1,28 +1,18 @@
 import { useNavigate } from 'react-router-dom'
-import { useAuthContext } from '../../hooks/useAuthContext'
-import { useCodexContext } from '../../hooks/useCodexContext'
 
 import styles from './css/Card.module.css'
 
 type CardProps = {
   link?: string
-  image: string
-  cardName?: string
+  image?: string
+  cardName: string
   size?: string
 }
 
 function Card({ link, image, cardName, size }: CardProps) {
-  const { user } = useAuthContext()
-  const { codex } = useCodexContext()
   const navigate = useNavigate()
 
   const handleView = () => {
-    // const linkUrl = codex?.codexName?.replace(/\s/g, '-').toLowerCase()
-    // console.log(linkUrl)
-
-    // if (linkUrl) {
-    //   navigate(`/${linkUrl}/${link}`)
-    // }
     navigate(`${link}`)
   }
 
